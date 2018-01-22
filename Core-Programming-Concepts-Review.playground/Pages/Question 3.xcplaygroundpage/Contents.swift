c98ffe96422e46a8d73b08b40ddede03abe88ec3
Bowler, Ryan <rbowler@lcs-mba2899.lcs.on.ca>
2018-01-22 13:53:34 -0500

    Question 3 corrected

--- Core-Programming-Concepts-Review.playground/Pages/Question 3.xcplaygroundpage/Contents.swift
+++ Core-Programming-Concepts-Review.playground/Pages/Question 3.xcplaygroundpage/Contents.swift
@@ -16,15 +16,15 @@
 // Answer question 3 below
 var piggyBank = 0
 
-piggyBank = piggyBank + 10
+piggyBank += 10
 print(piggyBank)
-piggyBank = piggyBank + 20
+piggyBank += 20
 print(piggyBank)
-piggyBank = piggyBank / 2
+piggyBank /= 2
 print(piggyBank)
-piggyBank = piggyBank * 3
+piggyBank *= 3
 print(piggyBank)
-piggyBank = piggyBank - 3
+piggyBank -= 3
 print(piggyBank)
 /*:
  ## Now share your understanding
